deeb13202b45554ee021c8b036df9be7c5ebcded
Tom Ecrepont <tomecrepont@gmail.com>
2021-05-24 11:13:30 +0200

    Creation of the Ingredient model

---
+++ db/migrate/20210524091313_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.integer :kind
+      t.integer :unit
+
+      t.timestamps
+    end
+  end
+end
