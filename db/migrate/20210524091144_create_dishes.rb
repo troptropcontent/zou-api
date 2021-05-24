c91875e8612070089649f15fc8b9b244e0835dbe
Tom Ecrepont <tomecrepont@gmail.com>
2021-05-24 11:12:10 +0200

    Creation of the Dish model

---
+++ db/migrate/20210524091144_create_dishes.rb
@@ -0,0 +1,15 @@
+class CreateDishes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :dishes do |t|
+      t.references :menu, null: false, foreign_key: true
+      t.integer :course
+      t.string :name
+      t.integer :price_excluding_tax_cents
+      t.integer :added_value_tax
+      t.integer :added_value_tax_amount_cents
+      t.string :price_including_tax_cents
+
+      t.timestamps
+    end
+  end
+end
